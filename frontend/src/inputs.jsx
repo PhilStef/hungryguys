9a60a530b891508b5b767c7294392a0cdabc4391
c-schutz <cschutz@ufl.edu>
2025-01-25 17:40:42 -0800

    Reverted checklist to a more simple design

--- frontend/src/inputs.jsx
+++ frontend/src/inputs.jsx
@@ -4,29 +4,25 @@ import './inputstyles.css';
 function Inputs() {
     return (
         <>
-            <div className="absolute right-0 pr-40 top-10">
-                <div class="justify">
-                    <label class="container">Gun Violence
-                        <input type="checkbox" />
-                        <span class="checkmark"></span>
-                    </label>
-
-                    <label class="container">Weather
-                        <input type="checkbox" />
-                        <span class="checkmark"></span>
-                    </label>
-
-                    <label class="container">Random
-                        <input type="checkbox" />
-                        <span class="checkmark"></span>
-                    </label>
-
-                    <label class="container">Random
-                        <input type="checkbox" />
-                        <span class="checkmark"></span>
-                    </label>
+            <fieldset className="absolute right-0 left-auto mx-auto top-10 p-4 border-2 border-gray-300">
+                <legend>Options</legend>
+                <div>
+                    <input type="checkbox" id="gunviolence"/>
+                    <label for="gunviolence">Gun Violence</label>
                 </div>
-            </div>
+                <div>
+                    <input type="checkbox" id="airquality"/>
+                    <label for="airquality">Air Quality</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="schools"/>
+                    <label for="schools">Schools</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="parks"/>
+                    <label for="parks">Parks</label>
+                </div>
+            </fieldset>
         </>
     )
 }
